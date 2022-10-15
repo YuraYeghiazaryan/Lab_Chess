c18276c969b8f9d417cca8e75249cb7315701b75
YuraYeghiazaryan <101919335+YuraYeghiazaryan@users.noreply.github.com>
2022-10-15 12:03:52 +0400

    Update pieces.py

--- pieces.py
+++ pieces.py
@@ -49,3 +49,111 @@ class King(Pieces):
 
 
 
+class Queen(Pieces):
+    def __init__(self, size_of_cell: int, color: str, field: str):
+        super().__init__(size_of_cell, color, field, '_queen.png')
+
+    '''def right_steps(self, cell_field_name):
+        for i in range(LTRS.index(cell_field_name[0] + 1), 0, -1):     #dzax verev ankyunagic
+            for j in range(int(cell_field_name[1] + 1), cells + 1):
+                self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i]+'j'])
+                if self.cells_and_name[LTRS[i]+'j'] in self.__all_pieces:
+                    break
+        for i in range(LTRS.index(cell_field_name[0]) + 1, cells):     #aj nerqev ankyunagic
+            for j in range(int(cell_field_name[1]) + 1, 0, -1):
+                self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i] + 'j'])
+                if self.cells_and_name[LTRS[i]+'j'] in self.__all_pieces:
+                    break
+        for i in range(LTRS.index(cell_field_name[0]) +1, cells):     # aj verev ankyunagic
+            for j in range(int(cell_field_name[1]) + 1, cells + 1):
+                self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i]+'j'])
+                if self.cells_and_name[LTRS[i]+'j'] in self.__all_pieces:
+                    break
+        for i in range(LTRS.index(cell_field_name[0]) + 1, 0, -1):   #dzax nerqev ankyunagic
+            for j in range(int(cell_field_name[1]) + 1, 0, -1):
+                self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i] + 'j'])
+                if self.cells_and_name[LTRS[i]+'j'] in self.__all_pieces:
+                    break
+        for i in range(int(cell_field_name[1]), cells + 1):           #verev
+            self.allowed_cells_for_steps.add(self.cells_and_name[cell_field_name[0] + 'i'])
+            if self.cells_and_name[cell_field_name[0] + 'i'] in self.__all_pieces:
+                break
+        for i in range(int(cell_field_name[1]), 0, -1):               #nerqev
+            self.allowed_cells_for_steps.add(self.cells_and_name[cell_field_name[0] + 'i'])
+            if self.cells_and_name[cell_field_name[0] + 'i'] in self.__all_pieces:
+                break
+        for i in range(int(cell_field_name[0]), 0, -1):               #dzax
+            self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i] + cell_field_name[1]])
+            if self.cells_and_name[cell_field_name[0] + 'i'] in self.__all_pieces:
+                break
+        for i in range(int(cell_field_name[1]), cells + 1):           #aj
+            self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i] + cell_field_name[1]])
+            if self.cells_and_name[cell_field_name[0] + 'i'] in self.__all_pieces:
+                break'''
+
+
+class Rook(Pieces):
+    def __init__(self, size_of_cell: int, color: str, field: str):
+        super().__init__(size_of_cell, color, field, '_rook.png')
+
+    '''def right_steps(self, cell_field_name):
+        for i in range(int(cell_field_name[1]), cells + 1):           #verev
+            self.allowed_cells_for_steps.add(self.cells_and_name[cell_field_name[0] + 'i'])
+            if self.cells_and_name[cell_field_name[0] + 'i'] in self.__all_pieces:
+                break
+        for i in range(int(cell_field_name[1]), 0, -1):               #nerqev
+            self.allowed_cells_for_steps.add(self.cells_and_name[cell_field_name[0] + 'i'])
+            if self.cells_and_name[cell_field_name[0] + 'i'] in self.__all_pieces:
+                break
+        for i in range(int(cell_field_name[0]), 0, -1):               #dzax
+            self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i] + cell_field_name[1]])
+            if self.cells_and_name[cell_field_name[0] + 'i'] in self.__all_pieces:
+                break
+        for i in range(int(cell_field_name[1]), cells + 1):           #aj
+            self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i] + cell_field_name[1]])
+            if self.cells_and_name[cell_field_name[0] + 'i'] in self.__all_pieces:
+                break'''
+
+
+class Knight(Pieces):
+    def __init__(self, size_of_cell: int, color: str, field: str):
+        super().__init__(size_of_cell, color, field, '_knight.png')
+
+    def right_steps(self, cell_field_name):
+        pass
+
+
+class Bishop(Pieces):
+    def __init__(self, size_of_cell: int, color: str, field: str):
+        super().__init__(size_of_cell, color, field, '_bishop.png')
+
+    '''def right_steps(self, cell_field_name):
+        for i in range(LTRS.index(cell_field_name[0] + 1), 0, -1):     #dzax verev ankyunagic
+            for j in range(int(cell_field_name[1] + 1), cells + 1):
+                self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i]+'j'])
+                if self.cells_and_name[LTRS[i]+'j'] in self.__all_pieces:
+                    break
+        for i in range(LTRS.index(cell_field_name[0]) + 1, cells):     #aj nerqev ankyunagic
+            for j in range(int(cell_field_name[1]) + 1, 0, -1):
+                self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i] + 'j'])
+                if self.cells_and_name[LTRS[i]+'j'] in self.__all_pieces:
+                    break
+        for i in range(LTRS.index(cell_field_name[0]) +1, cells):     # aj verev ankyunagic
+            for j in range(int(cell_field_name[1]) + 1, cells + 1):
+                self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i]+'j'])
+                if self.cells_and_name[LTRS[i]+'j'] in self.__all_pieces:
+                    break
+        for i in range(LTRS.index(cell_field_name[0]) + 1, 0, -1):   #dzax nerqev ankyunagic
+            for j in range(int(cell_field_name[1]) + 1, 0, -1):
+                self.allowed_cells_for_steps.add(self.cells_and_name[LTRS[i] + 'j'])
+                if self.cells_and_name[LTRS[i]+'j'] in self.__all_pieces:
+                    break'''
+
+
+class Pawn(Pieces):
+    def __init__(self, size_of_cell: int, color: str, field: str):
+        super().__init__(size_of_cell, color, field, '_pawn.png')
+
+    '''def right_steps(self, cell_field_name):
+        if cell_field_name[1] != cells:
+            self.allowed_cells_for_steps.add(self.cells_and_name[cell_field_name[0] + str(int(cell_field_name[1]) + 1)])'''
