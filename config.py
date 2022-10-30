b6c24e165dd96803fac0399b41664c156f56535c
YuraYeghiazaryan <101919335+YuraYeghiazaryan@users.noreply.github.com>
2022-10-31 02:53:38 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,12 @@ FPS = 10
 cells = 8
 cell_size = 57
 # COLORS = [(239, 219, 197), (130, 69, 19)]
+ACTIVE_COLOR = (250, 50, 50, 64)
 COLORS_FOR_BOARD = ['images/white_for_board.png', 'images/black_for_board.png']
 fnt_size = 18
 IMG_PATH = 'images/back.jpeg'
 LTRS = 'ABCDEFGH'
+next_step = ('w', 'b')
 PIECES_TYPES = {
     'k': ('King', 'b'), 'K': ('King', 'w'),
     'q': ('Queen', 'b'), 'Q': ('Queen', 'w'),
@@ -16,3 +18,13 @@ PIECES_TYPES = {
     'p': ('Pawn', 'b'), 'P': ('Pawn', 'w')
 }
 PIECE_PATH = 'images/pieces/'
+board = [
+    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+]
