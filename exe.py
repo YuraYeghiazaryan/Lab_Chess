7a59853a9973f4cb22f84de939610436219b10b6
YuraYeghiazaryan <101919335+YuraYeghiazaryan@users.noreply.github.com>
2022-10-31 02:54:32 +0400

    Update exe.py

--- exe.py
+++ exe.py
@@ -1,12 +1,14 @@
-from chess_items import *
+import chess_items
+import pygame as pg
+from config import *
 
 
 clock = pg.time.Clock()
 screen = pg.display.set_mode(win_size)
 pg.display.set_caption("Pair to pair Chess")
-screen.fill((150, 100, 25))
 
-chessboard = Chessboard(screen)
+chessboard = chess_items.Chessboard(screen)
+
 
 run = True
 while run:
@@ -17,6 +19,6 @@ while run:
             run = False
 
         if event.type == pg.MOUSEBUTTONDOWN:
-            chessboard.btn_down(event.button, event.pos)
+            chessboard.btn_down(event.pos)
         if event.type == pg.MOUSEBUTTONUP:
             chessboard.btn_up(event.button, event.pos)
