0e499ddfd2f9287f2ce5798abfccf7010425621a
YuraYeghiazaryan <101919335+YuraYeghiazaryan@users.noreply.github.com>
2023-04-18 11:16:16 +0400

    Delete client.py

--- client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('192.168.11.168', 8888))
-while True:
-    sock.send(input().encode('utf-8'))
-    data1 = sock.recv(1024)
-    print(data1.decode('utf-8'))
